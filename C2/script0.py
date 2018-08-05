065df6d7ed6b81fc0c5ea44cc6fdfc625219385f
jpch89 <jpch89@outlook.com>
2018-08-05 20:00:34 +0800

    add .gitignore and others

---
+++ C2/script0.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author: jpch89
+# @Email:  jpch89@outlook.com
+# @Date:   2018/7/21 8:59
+
+print('hello world')
+print(2 ** 100)
---
+++ C3/script1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author: jpch89
+# @Email:  jpch89@outlook.com
+# @Time:   2018/7/27 19:44
+
+# A first Python script
+import sys
+print(sys.platform)
+print(2 ** 100)
+x = 'Spam!'
+print(x * 8)
