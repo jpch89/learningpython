c20daeb74435aa827206f26b95063891880aecf7
jpch89 <jpch89@outlook.com>
2018-08-06 23:40:24 +0800

    20180806: 121 + 28 = 149

--- C3/threenames.py
+++ C3/threenames.py
@@ -2,3 +2,8 @@
 # @Author: jpch89
 # @Email:  jpch89@outlook.com
 # @Time:   2018/8/6 8:29
+
+a = 'dead'
+b = 'parrot'
+c = 'sketch'
+print(a, b, c)
--- EX1/module1.py
+++ EX1/module1.py
@@ -2,3 +2,5 @@
 # @Author: jpch89
 # @Email:  jpch89@outlook.com
 # @Time:   2018/8/6 13:23
+
+print("Hello module world!")
