ede48848b1badce23095795b7673f62354b0772b
进击的团子 <jpch89@outlook.com>
2019-01-15 11:06:41 +0800

    update code for chapter 29

---
+++ C29/docstr_test.py
@@ -0,0 +1,38 @@
+import docstr
+
+help(docstr)
+
+"""
+Help on module docstr:
+
+NAME
+    docstr - I am: docstr.__doc__
+
+CLASSES
+    builtins.object
+        spam
+
+    class spam(builtins.object)
+     |  I am: spam.__doc__ or docstr.spam.__doc__ or self.__doc__
+     |
+     |  Methods defined here:
+     |
+     |  method(self)
+     |      I am: spam.method.__doc__ or self.method.__doc__
+     |
+     |  ----------------------------------------------------------------------
+     |  Data descriptors defined here:
+     |
+     |  __dict__
+     |      dictionary for instance variables (if defined)
+     |
+     |  __weakref__
+     |      list of weak references to the object (if defined)
+
+FUNCTIONS
+    func(args)
+        I am: docstr.func.__doc__
+
+FILE
+    d:\code\learningpython\c29\docstr.py
+"""
